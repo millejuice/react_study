9d813cf1c15b17bc94cfce6ba3e9a1d7f15131d9
millejuice <potgusdavy@gmail.com>
2023-07-13 16:28:19 +0900

    state 끝

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let minutes;
 let hours;
-function App(){
+
+function MinutesToHours(){
   const [minutes,setMinutes] = React.useState(0);
   const [flipped,setFlipped] = React.useState(false);
   const onChange=(event) =>{
@@ -21,7 +21,6 @@ function App(){
   }
   return(
     <div>
-      <h3> Super Converter</h3>
       <label>Minutes</label>
       <input value = {flipped? minutes*60 : minutes}
       type = "number"
@@ -44,8 +43,34 @@ function App(){
   )
 }
 
+function KiloMToMm(){
+ return (
+ <h3>Kilometers To Milimeters</h3>
+ )
+}
 
+function App(){
+  const [index,setIndex] = React.useState("xx");
+  function onSelect(event){
+    setIndex(event.target.value);
+  }
+  return (
+    <div>
+      <h1>Super Converter</h1>
+      <select value={index} onChange={onSelect}>
+        <option value="xx">Select your values</option>
+        <option value="0">Minutes & Hours</option>
+        <option value="1">Kilo & miles</option>
+      </select>
+
+      {index == "xx" ? "select your value" : null}
+      {index == 0 ? <MinutesToHours /> : null}
+      {index == 1 ? <KiloMToMm /> : null}
+    </div>
+  )
+}
 root.render(<App />);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
