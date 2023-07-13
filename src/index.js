d452acdf719b54867d0629c5243fa226148f3944
millejuice <potgusdavy@gmail.com>
2023-07-13 15:18:45 +0900

    계산기 완성

--- src/index.js
+++ src/index.js
@@ -8,30 +8,42 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 let minutes;
 let hours;
 function App(){
-  const [minutes,setMinutes] = React.useState();
+  const [minutes,setMinutes] = React.useState(0);
+  const [flipped,setFlipped] = React.useState(false);
   const onChange=(event) =>{
     setMinutes(event.target.value);
   } 
+  function reset(){
+    setMinutes(0);
+  }
+  function onFlip(){
+    setFlipped((current)=>!current);
+  }
   return(
     <div>
       <h3> Super Converter</h3>
       <label>Minutes</label>
-      <input value = {minutes}
+      <input value = {flipped? minutes*60 : minutes}
       type = "number"
       id="minutes"
       onChange={onChange}
+      disabled={flipped}
       ></input>
       <div>
       <label>Hours</label>
-      <input value={minutes/60}
+      <input value={flipped ? minutes : Math.round(minutes/60)}
       type='number'
       id='hours'
       onChange={onChange}
+      disabled = {!flipped}
       ></input>
+      <button onClick={reset}>Reset</button>
+      <button onClick={onFlip}>Flip</button>
       </div>
     </div>
   )
 }
+
 
 root.render(<App />);
 // If you want to start measuring performance in your app, pass a function
